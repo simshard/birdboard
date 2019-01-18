a2843b947c9ecf8350c56eacd99bc4d7c33e13a6
simshard <30438951+simshard@users.noreply.github.com>
2019-01-18 17:24:50 +0000

    more  feature tests and a model/unit test of Users

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use App\Project;
 
 class ProjectsController extends Controller
@@ -14,13 +15,19 @@ class ProjectsController extends Controller
     }
     public function store()
     {
+
+    //  dd('its the store method');
       //validate
       $attributes=request()->validate([
         'title'=>'required',
         'description'=>'required',
     ]);
+    // $attributes['owner_id']=auth()->id;
+     auth()->user()->projects()->create($attributes);
+
+
       //persist
-      Project::create($attributes);
+    //  Project::create($attributes);
       //redirect
       return redirect('/projects');
     }
--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -11,22 +11,20 @@ class ProjectsTest extends TestCase
   use WithFaker,RefreshDatabase;
 
   /** @test */
-  public function a_user_can_create_a_project()
+  public function only_auth_users_can_create_a_project()
   {
-    $this->withoutExceptionHandling();
-    $attributes=[
-      'title'=>$this->faker->sentence,
-      'description'=>$this->faker->paragraph
-    ];
-    $this->post('/projects',$attributes)->assertRedirect('/projects');
-    $this->assertDatabaseHas('projects',$attributes);
-
-    $this->get('/projects')->assertSee($attributes['title']);
+    // $this->withoutExceptionHandling();
+    $attributes=factory('App\Project')->raw(['owner_id'=>null]);
+  //  $this->post('/projects',$attributes)->assertSessionHasErrors('owner_id');
+   $this->post('/projects',$attributes)->assertRedirect('login');
   }
+
+
 
   /** @test */
   public function a_project_requires_a_title()
   {
+    $this->actingAs(factory('App\User')->create());
     $attributes=factory('App\Project')->raw(['title'=>'']);
     $this->post('/projects',$attributes)->assertSessionHasErrors('title');
   }
@@ -34,11 +32,26 @@ class ProjectsTest extends TestCase
   /** @test */
   public function a_project_requires_a_description()
   {
-      $attributes=factory('App\Project')->raw(['description'=>'']);
+    $this->actingAs(factory('App\User')->create());
+    $attributes=factory('App\Project')->raw(['description'=>'']);
     $this->post('/projects',$attributes)->assertSessionHasErrors('description');
   }
 
+  /** @test */
+  public function a_user_can_create_a_project()
+  {
+   $this->withoutExceptionHandling();
+   $this->actingAs(factory('App\User')->create());
+    $attributes=[
+      'title'=>$this->faker->sentence,
+      'description'=>$this->faker->paragraph,
 
+    ];
+    $this->post('/projects',$attributes)->assertRedirect('/projects');
+    $this->assertDatabaseHas('projects',$attributes);
+
+    $this->get('/projects')->assertSee($attributes['title']);
+  }
   /** @test */
   public function a_user_can_view_a_project()
   {
