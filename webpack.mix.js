66bc26947acfb940824e6caa8605b1d3dd7ea3a6
simshard <30438951+simshard@users.noreply.github.com>
2019-02-03 20:10:28 +0000

    installed tailwind css and configured laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
+ var tailwindcss = require('tailwindcss');
+
+ 
+
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .options({
+     processCssUrls: false,
+     postCss: [ tailwindcss('./tailwind.js') ],
+   });
